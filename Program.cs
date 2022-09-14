15de75cc8a2b98f85e617c30cd4dfcd1c6ff7930
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-14 10:55:41 +0300

    application initialized

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fname;
+            Console.WriteLine("What is your name?");
+            fname=Console.ReadLine();
+            Console.WriteLine("How are you,"+fname);
+        }
+    }
+}
